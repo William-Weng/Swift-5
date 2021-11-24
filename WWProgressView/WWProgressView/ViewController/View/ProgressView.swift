b74ee9478329d8eae78e7afdf41cfaa9e2e78ab5
William.Weng <linuxice0609@gmail.com>
2021-11-24 16:00:05 +0800

    WWProgressView - 仿iOS13的音量調節元件 (加上新功能)

--- WWProgressView/WWProgressView/ViewController/View/ProgressView.swift
+++ WWProgressView/WWProgressView/ViewController/View/ProgressView.swift
@@ -12,18 +12,26 @@ protocol ProgressViewDeleagte: AnyObject {
         
     typealias ProgressViewInfomation = (text: String?, icon: UIImage?)
     
-    func valueChange(_ currentValue: CGFloat, maximumValue: CGFloat) -> ProgressViewInfomation
+    func valueChange(identifier: String, currentValue: CGFloat, maximumValue: CGFloat) -> ProgressViewInfomation
 }
 
 @IBDesignable final class ProgressView: UIView {
     
+    public enum ProgressType {
+        case continuous
+        case segmented(_ count: UInt)
+    }
+    
+    @IBInspectable var identifier: String = "ProgressView"
     @IBInspectable var currentValue: CGFloat = 0
     @IBInspectable var labelPosition: CGFloat = 0
     @IBInspectable var iconPosition: CGFloat = 0
     @IBInspectable var cornerRadius: CGFloat = 0
     @IBInspectable var borderWidth: CGFloat = 0
+    @IBInspectable var progressColor: UIColor = .clear
     @IBInspectable var borderColor: UIColor = .clear
 
+    @IBOutlet weak var progressView: UIView!
     @IBOutlet weak var displayLabel: UILabel!
     @IBOutlet weak var displayImageView: UIImageView!
     @IBOutlet weak var heightConstraint: NSLayoutConstraint!
@@ -34,8 +42,9 @@ protocol ProgressViewDeleagte: AnyObject {
 
     public weak var myDeleagte: ProgressViewDeleagte?
     
-    private var startTouchPoint: CGPoint?
-    
+    private var touchPoint: (start: CGPoint?, constant: CGFloat) = (nil, 0)
+    private var progressType: ProgressType = .continuous
+
     override public init(frame: CGRect) {
         super.init(frame: frame)
         initViewFromXib()
@@ -50,7 +59,7 @@ protocol ProgressViewDeleagte: AnyObject {
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) { recordStartTouchPoint(touches, with: event) }
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) { updateHeightConstraint(touches, with: event) }
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) { startTouchPoint = nil }
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) { touchPoint.start = nil }
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {}
 }
 
@@ -59,9 +68,15 @@ extension ProgressView {
     
     /// 設定一些初始的文字及圖示
     /// - Parameters:
-    ///   - initValue: String?
-    ///   - font: UIFont?
-    public func configure(initValue: String?, font: UIFont? = .systemFont(ofSize: 36.0), icon: UIImage? = nil) {
+    ///   - id: 辨識字
+    ///   - initValue: 上方的Label文字
+    ///   - font: Label字型
+    ///   - icon: 下方的圖示
+    ///   - type: 滑動的模式 (連續顯示 / 一段一段顯示)
+    public func configure(id: String, initValue: String?, font: UIFont? = .systemFont(ofSize: 36.0), icon: UIImage? = nil, type: ProgressType = .continuous) {
+        
+        identifier = id
+        progressType = type
         
         displayLabel.font = font
         displayLabel.text = initValue
@@ -91,7 +106,9 @@ extension ProgressView {
         layer.borderWidth = borderWidth
         layer.borderColor = borderColor.cgColor
         
-        heightConstraint.constant = currentValue
+        progressView.backgroundColor = progressColor
+        touchPoint.constant = safeTouchPointConstant(currentValue)
+        heightConstraint.constant = touchPoint.constant
         iconPositionConstraint.constant = iconPosition
         labelPositionConstraint.constant = labelPosition
     }
@@ -111,37 +128,72 @@ extension ProgressView {
     ///   - event: UIEvent?
     private func recordStartTouchPoint(_ touches: Set<UITouch>, with event: UIEvent?) {
         guard let touch = touches.first else { return }
-        startTouchPoint = touch.location(in: self)
+        touchPoint.start = touch.location(in: self)
     }
     
-    /// 更新高度Constraint
+    /// 更新高度Constraint (range: min~max)
     /// - Parameters:
     ///   - touches: Set<UITouch>
     ///   - event: UIEvent?
     private func updateHeightConstraint(_ touches: Set<UITouch>, with event: UIEvent?) {
         
-        guard let startTouchPoint = startTouchPoint,
+        guard let startTouchPoint = touchPoint.start,
               let endTouchPoint = touches.first?.location(in: self)
         else {
             return
         }
         
-        heightConstraint.constant += startTouchPoint.y - endTouchPoint.y
-        self.startTouchPoint = endTouchPoint
-        
-        if (heightConstraint.constant > contentView.frame.height) { heightConstraint.constant = contentView.frame.height }
-        if (heightConstraint.constant < 0) { heightConstraint.constant = 0 }
-
+        touchPoint.start = endTouchPoint
+        touchPoint.constant += startTouchPoint.y - endTouchPoint.y
+        touchPoint.constant = safeTouchPointConstant(touchPoint.constant)
+                
+        heightConstraint.constant = safeHeightConstraint(touchPoint.constant, progressType: progressType)
         valueChangeAction(heightConstraint.constant)
+    }
+    
+    /// 設定數值的安全大小範圍 (記錄的高度) => 0 ~ 最大高度
+    /// - Parameter touchPointConstant: CGFloat
+    /// - Returns: CGFloat
+    private func safeTouchPointConstant(_ touchPointConstant: CGFloat) -> CGFloat {
+        
+        if (touchPointConstant > contentView.frame.height) { return contentView.frame.height }
+        if (touchPointConstant < 0) { return 0 }
+        
+        return touchPointConstant
+    }
+    
+    /// 設定數值的安全大小範圍 (顯示的高度) => 0 ~ 最大高度
+    /// - Parameter touchPointConstant: CGFloat
+    /// - Returns: CGFloat
+    private func safeHeightConstraint(_ touchPointConstant: CGFloat, progressType: ProgressType) -> CGFloat {
+        
+        let _touchPointConstant = safeTouchPointConstant(touchPointConstant)
+        
+        switch progressType {
+        case .continuous: return _touchPointConstant
+        case .segmented(let count): return segmentedHeightConstraint(count, touchPointConstant: touchPointConstant)
+        }
     }
     
     /// 數值改變時的反應
     /// - Parameter value: CGFloat?
     private func valueChangeAction(_ value: CGFloat) {
         
-        guard let info = myDeleagte?.valueChange(value, maximumValue: contentView.frame.height) else { return }
+        guard let info = myDeleagte?.valueChange(identifier: identifier, currentValue: value, maximumValue: contentView.frame.height) else { return }
         
         displayLabel.text = info.text
         displayImageView.image = info.icon
+    }
+    
+    /// 產生出分段的高度Constraint (一格一格的)
+    /// - Parameter count: UInt
+    /// - Parameter touchPointConstant: 真正的所點的高度
+    /// - Returns: CGFloat?
+    private func segmentedHeightConstraint(_ count: UInt, touchPointConstant: CGFloat) -> CGFloat {
+        
+        let index = Int(touchPointConstant * CGFloat(count) / contentView.frame.height)
+        let result = contentView.frame.height * CGFloat(index) / CGFloat(count)
+                
+        return result
     }
 }
--- WWProgressView/WWProgressView/ViewController/ViewController.swift
+++ WWProgressView/WWProgressView/ViewController/ViewController.swift
@@ -11,23 +11,39 @@ import WWPrint
 final class ViewController: UIViewController {
     
     @IBOutlet weak var myProgressView: ProgressView!
+    @IBOutlet weak var myProgressView2: ProgressView!
+    @IBOutlet weak var myProgressView3: ProgressView!
+
+    private let count: UInt = 10
+    private let icon: (continuous: UIImage, segmented: UIImage) = (#imageLiteral(resourceName: "SoundOn"), #imageLiteral(resourceName: "WifiOn"))
+    
+    private enum Identifier: String {
+        case continuous = "continuous"
+        case segmented = "segmented"
+        case segmented2 = "segmented2"
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let icon = #imageLiteral(resourceName: "SoundOn")
         myProgressView.myDeleagte = self
+        myProgressView2.myDeleagte = self
+        myProgressView3.myDeleagte = self
+
+        myProgressView.configure(id: "\(Identifier.continuous)", initValue: "50%", font: .systemFont(ofSize: 16), icon: icon.continuous, type: .continuous)
+        myProgressView2.configure(id: "\(Identifier.segmented)", initValue: "5/\(count)", font: .systemFont(ofSize: 16), icon: icon.segmented, type: .segmented(count))
+        myProgressView3.configure(id: "\(Identifier.segmented2)", initValue: "0", font: .systemFont(ofSize: 16), icon: icon.segmented, type: .segmented(count))
         
         #if DEBUG
-        myProgressView.configure(initValue: "1 / 5", font: .systemFont(ofSize: 36), icon: icon)
+        wwPrint("DEBUG")
         #endif
         
         #if RELEASE
-        myProgressView.configure(initValue: nil, font: .systemFont(ofSize: 10), icon: icon)
+        wwPrint("RELEASE")
         #endif
         
         #if STAGE
-        myProgressView.configure(initValue: "50 %", font: .systemFont(ofSize: 20), icon: icon)
+        wwPrint("STAGE")
         #endif
     }
 }
@@ -35,7 +51,22 @@ final class ViewController: UIViewController {
 // MARK: - ProgressViewDeleagte
 extension ViewController: ProgressViewDeleagte {
     
-    func valueChange(_ currentValue: CGFloat, maximumValue: CGFloat) -> ProgressViewDeleagte.ProgressViewInfomation {
+    func valueChange(identifier: String, currentValue: CGFloat, maximumValue: CGFloat) -> ProgressViewDeleagte.ProgressViewInfomation {
+        
+        guard let identifier = Identifier(rawValue: identifier) else { fatalError() }
+        
+        switch identifier {
+        case .continuous: return continuousTypeInfo(currentValue: currentValue, maximumValue: maximumValue)
+        case .segmented: return segmentedTypeInfo(currentValue: currentValue, maximumValue: maximumValue)
+        case .segmented2: return segmentedTypeInfo2(currentValue: currentValue, maximumValue: maximumValue)
+        }
+    }
+}
+
+extension ViewController {
+    
+    /// 連續的顯示效果
+    private func continuousTypeInfo(currentValue: CGFloat, maximumValue: CGFloat) -> ProgressViewDeleagte.ProgressViewInfomation {
         
         let percentage = Int(currentValue / maximumValue * 100)
         var icon = #imageLiteral(resourceName: "SoundOff")
@@ -47,5 +78,35 @@ extension ViewController: ProgressViewDeleagte {
         }
         
         return (text: "\(percentage) %", icon: icon)
+    }
+    
+    /// 分段的顯示效果
+    private func segmentedTypeInfo(currentValue: CGFloat, maximumValue: CGFloat) -> ProgressViewDeleagte.ProgressViewInfomation {
+        
+        let index = Int(currentValue * CGFloat(count) / maximumValue)
+        var icon = #imageLiteral(resourceName: "WifiOff")
+        
+        switch index {
+        case 3...6: icon = #imageLiteral(resourceName: "WifiOn")
+        case 7...: icon = #imageLiteral(resourceName: "WifiFull")
+        default: icon = #imageLiteral(resourceName: "WifiOff")
+        }
+        
+        return (text: "\(index)/\(count)", icon: icon)
+    }
+    
+    /// 分段的顯示效果2
+    private func segmentedTypeInfo2(currentValue: CGFloat, maximumValue: CGFloat) -> ProgressViewDeleagte.ProgressViewInfomation {
+        
+        let level = Int(currentValue * CGFloat(count) / maximumValue) - Int(count) / 2
+        var icon = #imageLiteral(resourceName: "WifiOff")
+        
+        switch level {
+        case (-5)...(-2): icon = #imageLiteral(resourceName: "BulbOff")
+        case (-1)...2: icon = #imageLiteral(resourceName: "BulbOn")
+        default: icon = #imageLiteral(resourceName: "BulbFull")
+        }
+        
+        return (text: "\(level)", icon: icon)
     }
 }
