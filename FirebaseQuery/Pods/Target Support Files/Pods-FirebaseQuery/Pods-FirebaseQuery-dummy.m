bf5a361badb97c0dfd2db2d225c7a5a6f925c11c
William.Weng <linuxice0609@gmail.com>
2022-01-22 17:28:11 +0800

    Delete Pods

--- FirebaseQuery/Pods/Target Support Files/Pods-FirebaseQuery/Pods-FirebaseQuery-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_FirebaseQuery : NSObject
-@end
-@implementation PodsDummy_Pods_FirebaseQuery
-@end
--- Firebase_HelloWorld/Pods/Target Support Files/Pods-Firebase_HelloWorld/Pods-Firebase_HelloWorld-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Firebase_HelloWorld : NSObject
-@end
-@implementation PodsDummy_Pods_Firebase_HelloWorld
-@end
